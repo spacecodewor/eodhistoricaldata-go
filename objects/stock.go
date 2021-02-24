c6205d1313bcdce4cbab665b00441bcfd8c2a506
Churbakov Igor <churbakov.igor@wildberries.ru>
2021-02-24 18:54:46 +0300

    Fix

--- objects/stock.go
+++ objects/stock.go
@@ -1,15 +1,16 @@
 package objects
 
+// Quote ...
 type Quote struct {
-	Code          string  `json:"code"`
-	Timestamp     int64   `json:"timestamp"`
-	Gmtoffset     int64   `json:"gmtoffset"`
-	Open          float64 `json:"open"`
-	High          float64 `json:"high"`
-	Low           float64 `json:"low"`
-	Close         float64 `json:"close"`
-	Volume        int64   `json:"volume"`
-	PreviousClose float64 `json:"previousClose"`
-	Change        float64 `json:"change"`
-	ChangeP       float64 `json:"change_p"`
+	Code          string      `json:"code"`
+	Timestamp     int64       `json:"timestamp"`
+	Gmtoffset     int64       `json:"gmtoffset"`
+	Open          float64     `json:"open"`
+	High          float64     `json:"high"`
+	Low           float64     `json:"low"`
+	Close         float64     `json:"close"`
+	Volume        interface{} `json:"volume"` // int64 || "NA"
+	PreviousClose float64     `json:"previousClose"`
+	Change        float64     `json:"change"`
+	ChangeP       float64     `json:"change_p"`
 }
